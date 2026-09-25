fffdd7bb68883558ed563c3d5bd6c21318a15eb7
Serena <94026305+serenakeyitan@users.noreply.github.com>
2026-09-24 20:23:33 -0700

    Give the mobile diagram toolbar the full dialog width

---
+++ server/runtime/assets/swimlanesDiagram-VR7AAH4N-De4wynMV.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-9hOC9Tqg.js";import{_ as a}from"./index-woIJjS3X.js";import"./chunk-5VM5RSS4-BwbaKrBu.js";import"./chunk-XXDRQBXY-BcxjnHDK.js";import"./chunk-POPQ4Y6H-Da1sndOl.js";import"./chunk-F27PBJKO-CW5Ys629.js";import"./channel-BqKes3ec.js";import"../shell.6hDrgHIS.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
