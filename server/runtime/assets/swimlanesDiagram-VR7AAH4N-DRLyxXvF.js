815d3ccd8d626ea6f545183faa1de546d6514ece
Serena <94026305+serenakeyitan@users.noreply.github.com>
2026-09-24 22:19:49 -0700

    Balance visible spacing on device approval screens

--- server/runtime/assets/swimlanesDiagram-VR7AAH4N-DRLyxXvF.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-DZwK7ie1.js";import{_ as a}from"./index-C5LSkQnw.js";import"./chunk-5VM5RSS4-CnG5faTZ.js";import"./chunk-XXDRQBXY-B8-4arEg.js";import"./chunk-POPQ4Y6H--XlzTEkF.js";import"./chunk-F27PBJKO-CUkJR7yF.js";import"./channel-CHJvrav1.js";import"../shell.BsAGk2E9.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
