d2f1721756dd474d4667406da4739f87943c3842
Serena <94026305+serenakeyitan@users.noreply.github.com>
2026-09-24 22:46:46 -0700

    Center supporting copy within the sign-in text column

---
+++ server/runtime/assets/swimlanesDiagram-VR7AAH4N-COZi-2Im.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-BIBgnbap.js";import{_ as a}from"./index-EYEdNCqk.js";import"./chunk-5VM5RSS4-CoUx-tpt.js";import"./chunk-XXDRQBXY-C-xUak9L.js";import"./chunk-POPQ4Y6H-By5ox4JX.js";import"./chunk-F27PBJKO-Z1S5E-HC.js";import"./channel-Bbxzow9H.js";import"../shell.Dyyu4J0m.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
